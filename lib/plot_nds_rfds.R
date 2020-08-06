7007b4658dd503e8dbf363d1844596f78fb7a063
Zena Lapp <zenalapp@gmail.com>
2020-08-06 15:00:20 -0400

    Unfacet nd-rfd plot

--- lib/plot_nds_rfds.R
+++ lib/plot_nds_rfds.R
@@ -5,7 +5,19 @@ theme_set(theme_bw())
 all_results <- read_csv('results/all_results.csv')
 
 p <- all_results %>% 
-  filter(!is.na(nd) & !is.na(rfd) & error < 100 & !is.infinite(nd) & !is.infinite(rfd) & rfd < 50) %>% 
-  ggplot(aes(x=nd,y=rfd,col=temp,alpha=1/log10(error))) + geom_jitter() + facet_wrap(vars(temp))
+  filter(!is.na(nd) & !is.na(rfd) & 
+           #error < 100 & 
+           !is.infinite(nd) & !is.infinite(rfd) & 
+           rfd < 10 & a_ij > 0 & a_ji > 0) %>% 
+  ggplot(aes(x=nd,y=rfd,col=temp,alpha=1/log10(error))) + geom_jitter() #+ facet_wrap(vars(temp))
 
 ggsave(p, file = 'results/figures/nd_rfd/nd_rfd.pdf')
+
+# all_results %>% 
+#   filter(!is.na(nd) & !is.na(rfd) & 
+#            error < 50 & 
+#            !is.infinite(nd) & !is.infinite(rfd) & 
+#            rfd < 10 & a_ij > 0 & a_ji > 0) %>% 
+#   group_by(temp) %>% summarize(nd_mean=mean(nd),rfd_mean=mean(rfd)) %>% ggplot(aes(x=nd_mean,y=rfd_mean,col=temp)) + geom_point(size=5)
+
+#all_results %>% group_by(temp) %>% filter(error == min(error,na.rm=T))
